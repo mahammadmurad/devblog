3c3e29420f2da7abb9169e7b46cc5c4c9adfd7d8
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-16 03:05:51 +0400

    first commit

---
+++ projects/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from .models import Project, Tag
+from .forms import ProjectForm 
+
+
+def projects(request):
+    projects = searchProjects(request)
+    custom_range, projects = paginateProjects(request, projects, 6)
+
+    context = {'projects': projects,
+               'search_query': search_query, 'custom_range': custom_range}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        projectObj.getVoteCount
+
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'form': form})
+
+def createProject(request):
+    profile = request.user.profile
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',',  " ").split()
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
