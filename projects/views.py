a8eaf478127442919cb5460f8cb14805621458a7
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-17 22:35:12 +0400

    forms.py created

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project, Tag
 from .forms import ProjectForm 
 
@@ -31,21 +31,40 @@ def project(request, pk):
     return render(request, 'projects/single-project.html', {'project': projectObj, 'form': form})
 
 def createProject(request):
-    profile = request.user.profile
     form = ProjectForm()
 
     if request.method == 'POST':
-        newtags = request.POST.get('newtags').replace(',',  " ").split()
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            project = form.save(commit=False)
-            project.owner = profile
-            project.save()
-
-            for tag in newtags:
-                tag, created = Tag.objects.get_or_create(name=tag)
-                project.tags.add(tag)
-            return redirect('account')
+            form.save()
+            return redirect('projects')
 
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
+
+
+def updateProject(request, pk):
+    
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST , request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    
+    context = {'object': project}
+    return render(request, 'delete_template.html', context)
+
+
+
+# Create your views here.
