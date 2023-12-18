d99493685f590c40c5140c014043e7ee48270bb4
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-18 22:59:30 +0400

    signals created

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(
