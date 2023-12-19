faa798e2a0626539f56a001295a66924fe0815b0
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-19 18:51:54 +0400

    account created

--- users/views.py
+++ users/views.py
@@ -87,4 +87,31 @@ def userProfile(request, pk):
                "otherSkills": otherSkills}
     return render(request, 'users/user-profile.html', context)
 
-# Create your views here.
+
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+
+    context = {'profile': profile, 'skills': skills, 'projects': projects}
+
+    return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
