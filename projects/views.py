0234053fdc566e01f65178b9f6c2af365104a6da
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-21 00:31:10 +0400

    add-edit-delete skills views created

--- projects/views.py
+++ projects/views.py
@@ -34,10 +34,26 @@ def project(request, pk):
 
 @login_required(login_url="login")
 def createProject(request):
+    profile = request.user.profile
     form = ProjectForm()
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+@login_required(login_url="login")
+def updateProject(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST , request.FILES, instance=project)
         if form.is_valid():
             form.save()
             return redirect('projects')
@@ -45,30 +61,19 @@ def createProject(request):
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
-#@login_required(login_url="login")
-# def updateProject(request, pk):
-    
-#     project = Project.objects.get(id=pk)
-#     form = ProjectForm(instance=project)
-#     if request.method == 'POST':
-#         form = ProjectForm(request.POST , request.FILES, instance=project)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('projects')
 
-#     context = {'form': form}
-#     return render(request, "projects/project_form.html", context)
+@login_required(login_url="login")
+def deleteProject(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'delete_template.html', context)
 
 
-#@login_required(login_url="login")
-# def deleteProject(request, pk):
-#     project = Project.objects.get(id=pk)
-#     if request.method == 'POST':
-#         project.delete()
-#         return redirect('projects')
-    
-#     context = {'object': project}
-#     return render(request, 'delete_template.html', context)
+
 
 
 
