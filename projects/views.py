0bcc3d4e5d5d8a300fa6a993098d30325361ccb7
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-26 02:18:08 +0400

    review system created

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Project, Tag
-from .forms import ProjectForm 
+from .forms import ProjectForm , ReviewForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .utils import searchProjects, paginateProjects
 from django.core.paginator import Paginator, PageNotAnInteger,EmptyPage
+from django.contrib import messages
 
 
 def projects(request):
@@ -22,21 +23,21 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-#     form = ReviewForm()
+    form = ReviewForm()
 
-#     if request.method == 'POST':
-#         form = ReviewForm(request.POST)
-#         review = form.save(commit=False)
-#         review.project = projectObj
-#         review.owner = request.user.profile
-#         review.save()
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save() 
 
-#         projectObj.getVoteCount
+        projectObj.getVoteCount
 
-#         messages.success(request, 'Your review was successfully submitted!')
-#         return redirect('project', pk=projectObj.id)
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
 
-    return render(request, 'projects/single-project.html', {'project': projectObj,})
+    return render(request, 'projects/single-project.html', {'project': projectObj,'form':form})
 
 @login_required(login_url="login")
 def createProject(request):
